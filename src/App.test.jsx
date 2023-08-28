12ecdc56934a8501b04bb2993be99cfda080e112
Ivan Aimufua <ivanaimufua41@gmail.com>
2023-08-27 19:24:56 -0600

    wip: add styled components into the project

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import axiosMockAdapter from './testUtils/mockAdapter'
 import App from './App';
 
 test('renders learn react link', () => {
+  // axiosMockAdapter.onGet
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
